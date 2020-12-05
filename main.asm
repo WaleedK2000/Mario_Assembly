bd8c52e388188cfb8e97957ec28ca2b67343cb7a
WaleedK2000 <74948618+WaleedK2000@users.noreply.github.com>
2020-12-05 15:51:45 +0500

    added some lines

--- main.asm
+++ main.asm
@@ -1,9 +1,29 @@
+include macro.inc
 
 .model small
-.486
+.386
 .stack 0100h
 
 .data
 
 .code
 
+	mov ax, @data
+	mov ds, ax
+	mov ax, 0
+	jmp main
+	
+	
+	
+	
+	main proc
+	
+		mov ax, 253
+		
+		mTest
+	
+	main endp
+	
+	mov ah, 4ch
+	int 21h
+	end
